f1b9f55065ed35b45bbc36260b6b325c1fc78b69
Lena Zvereva <zverolen@gmail.com>
2024-04-11 10:54:13 +0200

    Add styles for header and controls

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom"
 
+import styles from './Header.module.css'
+
 export default function Header() {
   return (
-    <header>
-      <h1>Знаю!</h1>
+    <header className={styles.header}>
+      <h1><NavLink className="link logo" to="/"><span>Знаю!</span></NavLink></h1>
       <div>
-        <button>Инструкция</button>
-        <span>|</span>
-        <NavLink to="/account">Войти</NavLink>
+        <button className="link">Инструкция</button>
+        <NavLink className="link" to="/account"><span>Аккаунт</span></NavLink>
       </div>
     </header>
   )
