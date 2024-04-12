439cb3b95c2cb8b19040583a5b36ca35a9daf41f
Lena Zvereva <zverolen@gmail.com>
2024-04-12 11:31:09 +0200

    Add small changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className={styles.header}>
       <h1><NavLink className="link logo" to="/"><span>Знаю!</span></NavLink></h1>
       <div>
-        <button className="link">Инструкция</button>
+        {/* <button className="link">Инструкция</button> */}
         <NavLink className="link" to="/account"><span>Аккаунт</span></NavLink>
       </div>
     </header>
